12cf6fc446f02da333fde3964a7182bb64a994af
rajeevkommana <rajeevkommana1990@gmail.com>
2019-01-04 19:13:56 +0530

    first commit

---
+++ AlertBox.java
@@ -0,0 +1,11 @@
+package newjavaexp;
+
+import javax.swing.JOptionPane;
+
+public class AlertBox {
+	
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hello Java", "Java Alert", JOptionPane.INFORMATION_MESSAGE);
+	}
+
+}
