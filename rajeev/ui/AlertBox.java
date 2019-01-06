d0c8361a061c5fd67a2c128924ccd56033a5a90e
rajeevkommana <rajeevkommana1990@gmail.com>
2019-01-06 19:12:11 +0530

    sasikar package name changed to rajeev

---
+++ rajeev/ui/AlertBox.java
@@ -0,0 +1,10 @@
+package sasikar.ui;
+import javax.swing.JOptionPane;
+
+public class AlertBox {
+	
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hello Java", "Java Alert", JOptionPane.INFORMATION_MESSAGE);
+	}
+
+}
