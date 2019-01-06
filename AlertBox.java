49b105ba759398920ba9f8b4c0079b0cfd1a2e5d
rajeevkommana <rajeevkommana1990@gmail.com>
2019-01-06 18:52:28 +0530

    Delete AlertBox.java

--- AlertBox.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class AlertBox {
-	
-	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "Hello Java", "Java Alert", JOptionPane.INFORMATION_MESSAGE);
-	}
-
-}
